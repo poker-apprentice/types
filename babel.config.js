2723dacd9695dff19acfad251ad11e84e92dc8d6
Matt Huggins <matt.huggins@gmail.com>
2023-10-08 21:11:39 -0500

    chore: remove unused import

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
   plugins: ['@babel/plugin-transform-runtime'],
