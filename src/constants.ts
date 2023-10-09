d85e5675747826fd82c3263bbd9fc2838c8e167b
Matt Huggins <matt.huggins@gmail.com>
2023-10-08 21:20:41 -0500

    chore: ensure ALL_SUITS & ALL_RANKS include all possible values

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,37 @@
 import type { Card, Rank, Suit } from './types';
 
-export const ALL_RANKS: Rank[] = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+type AtLeastOne<T> = [T, ...T[]];
 
-export const ALL_SUITS: Suit[] = ['c', 'd', 'h', 's'];
+// Ensures an array contains every possible value of a string union.
+const exhaustiveStringTuple =
+  <T extends string>() =>
+  <L extends AtLeastOne<T>>(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ...x: L extends never
+      ? never
+      : Exclude<T, L[number]> extends never
+      ? L
+      : Exclude<T, L[number]>[]
+  ) =>
+    x;
+
+export const ALL_RANKS: Rank[] = exhaustiveStringTuple<Rank>()(
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'T',
+  'J',
+  'Q',
+  'K',
+  'A',
+);
+
+export const ALL_SUITS: Suit[] = exhaustiveStringTuple<Suit>()('c', 'd', 'h', 's');
 
 export const ALL_CARDS = ALL_RANKS.reduce((acc: Card[], rank) => {
   acc.push(...ALL_SUITS.map((suit) => `${rank}${suit}` as Card));
